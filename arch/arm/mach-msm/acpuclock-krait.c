71a4af75bad565c95ab68f78ae3fa703e421c618
rmbq <davide.rombola@gmail.com>
2013-11-08 21:18:36 +0100

    msm: acpuclock-krait: bypass qualcomm Vmin patch
    * thanks to @jrior001 for the hint
    
    Change-Id: I4af4e04f3593dbd481a40fcbabe78305235a26ea

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -1105,6 +1105,9 @@ static struct notifier_block __cpuinitdata acpuclk_cpu_notifier = {
 
 static const int __init krait_needs_vmin(void)
 {
+#ifdef CONFIG_BYPASS_KRAIT_NEEDS_VMIN
+	return 0;
+#else
 	switch (read_cpuid_id()) {
 	case 0x511F04D0: /* KR28M2A20 */
 	case 0x511F04D1: /* KR28M2A21 */
@@ -1113,6 +1116,7 @@ static const int __init krait_needs_vmin(void)
 	default:
 		return 0;
 	};
+#endif
 }
 
 static void __init krait_apply_vmin(struct acpu_level *tbl)
