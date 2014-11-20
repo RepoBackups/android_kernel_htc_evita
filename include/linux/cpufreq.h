869b02e6e11f605c3a2191662329f83037818a16
Paul Reioux <reioux@gmail.com>
2014-11-20 11:24:57 -0600

    cpufreq: add intellimm governor
    intelliminmax (intellimm) governor is designed to work with the newer SOCs
    with fixed voltage rails (ie MSM8974+ SOCs).
    
    It is designed to work within those
    fixed voltage ranges in order to maximize battery performance while creating a
    smooth UI operations.
    
    Change-Id: Iba77751d81287106795dfae629d19875e3619470
    Copyright 2014 Paul Reioux <aka faux123>
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -449,6 +449,9 @@ extern struct cpufreq_governor cpufreq_gov_abyssplugv2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUG)
 extern struct cpufreq_governor cpufreq_gov_abyssplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_abyssplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
+extern struct cpufreq_governor cpufreq_gov_intellimm;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellimm)
 #endif
 
 
