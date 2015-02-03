a017ed21ec4dea825d0977b15570e4310ecd57b7
Dave Savage <bedalus@gmail.com>
2015-02-03 18:28:26 +0000

    cpufreq: add preservative governor
    Change-Id: Ie926f02f7aab1a4bba5c0f0a57658fb0f90f9f61
    Signed-off-by: Dave Savage <bedalus@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -434,6 +434,9 @@ extern struct cpufreq_governor cpufreq_gov_adaptive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PRESERVATIVE)
+extern struct cpufreq_governor cpufreq_gov_preservative;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_preservative)
 #endif
 
 
